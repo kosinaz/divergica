03b105f2751773ac9619e2b1fbd0b9722cf21e0c
Kosina Zoltán <kosinaz@gmail.com>
2024-02-15 17:02:51 +0100

    add timed brush movement

--- divergica/scenes/level1.gd
+++ divergica/scenes/level1.gd
@@ -13,37 +13,45 @@ func _ready():
 
 func _start_round():
 	$"PaintButton".show()
+	$"FollowTimer".start()
 	$"BrushAnimationPlayer".play("0to1")
-	current_mushroom = 0
+	current_mushroom = -1
 
 func _on_paint_button_pressed():
-	if current_mushroom > 3:
+	if $"FollowTimer".time_left < 0.5 or current_mushroom == -1:
+		$"Brush".get_node("Error").play()
 		return
 	var mushroom_name = $"Mushrooms".get_child(current_mushroom).name
 	if mushroom_name.begins_with("Black"):
 		$"Brush".get_node("Error").play()
-		current_mushroom += 1
 		return
 	$"Brush".get_child(notes[current_mushroom]).play()
 	if mushroom_name.begins_with("Striped"):
-		print($"Mushrooms".get_child(current_mushroom).get_node("AnimatedSprite").animation)
 		if $"Mushrooms".get_child(current_mushroom).get_node("AnimatedSprite").animation == "once":
-			print("yes")
 			$"Mushrooms".get_child(current_mushroom).get_node("AnimatedSprite").play("twice")
-			current_mushroom += 2
 		else:
 			$"Mushrooms".get_child(current_mushroom).get_node("AnimatedSprite").play("once")
 	if mushroom_name.begins_with("White"):
 		$"Mushrooms".get_child(current_mushroom).get_node("AnimatedSprite").play("painted")
-		current_mushroom += 1
-	print(current_mushroom)
 
-func _on_timer_timeout():
+func _on_lead_timer_timeout():
 	if current_mushroom > 3:
-		$"Timer".stop()
+		$"LeadTimer".stop()
 		_start_round()
 		return
 	if not $"Mushrooms".get_child(current_mushroom).name.begins_with("None"):
 		$"MushroomSounds".get_child(notes[current_mushroom]).play()
 		$"Mushrooms".get_child(current_mushroom).get_node("AnimationPlayer").play("default")
 	current_mushroom += 1
+
+func _on_follow_timer_timeout():
+	current_mushroom += 1
+	if current_mushroom == 0:
+		$"BrushAnimationPlayer".play("1to2")
+	if current_mushroom == 1:
+		$"BrushAnimationPlayer".play("2to3")
+	if current_mushroom == 2:
+		$"BrushAnimationPlayer".play("3to4")
+	if current_mushroom > 3:
+		$"FollowTimer".stop()
+		$"PaintButton".hide()
